649dde2ebc3f8d2e68a4a68cb52fc5cc725eaefc
Kenny Root <kroot@google.com>
2014-06-20 18:16:47 -0700

    Conditionalize RIPEMD references
    This unconditionally includes RIPEMD, but it's the only place in the
    Android source code base. Instead of restoring RIPEMD to OpenSSL, just
    remove it here.
    
    Change-Id: Ie949f64cef1cb7de39baae3f118f2188e663c875

--- mac.c
+++ mac.c
@@ -65,8 +65,10 @@ struct {
 #endif
 	{ "hmac-md5",			SSH_EVP, EVP_md5, 0, -1, -1 },
 	{ "hmac-md5-96",		SSH_EVP, EVP_md5, 96, -1, -1 },
+#ifdef HAVE_EVP_RIPEMD
 	{ "hmac-ripemd160",		SSH_EVP, EVP_ripemd160, 0, -1, -1 },
 	{ "hmac-ripemd160@openssh.com",	SSH_EVP, EVP_ripemd160, 0, -1, -1 },
+#endif
 	{ "umac-64@openssh.com",	SSH_UMAC, NULL, 0, 128, 64 },
 	{ NULL,				0, NULL, 0, -1, -1 }
 };
